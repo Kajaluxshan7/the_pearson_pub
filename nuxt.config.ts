20b240e1a720e3173e600495c4e2c76c1bbdf63b
Kajaluxshan7 <kajalux-se19005@stu.kln.ac.lk>
2025-03-29 23:10:45 +0530

    initial_commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-03-29',
+  css: ['~/assets/styles/index.css']
+,
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Welcome to The Pearson Pub</h1>
+    <p>This is your Nuxt 3 project homepage.</p>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "HomePage",
+});
+</script>
+
