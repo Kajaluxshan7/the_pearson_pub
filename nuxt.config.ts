ba0288dd38abc2e0c2aa74bd8239cb84a73302e7
Kajaluxshan7 <kajalux-se19005@stu.kln.ac.lk>
2025-03-29 23:26:53 +0530

    check_deployment_status

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-03-29',
   css: ['~/assets/styles/index.css']
 ,
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/the_pearson_pub/' // Replace with your repo name
+  }
 })
